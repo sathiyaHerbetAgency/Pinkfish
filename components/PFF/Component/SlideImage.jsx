f7dc2536ecdd9cbf5eddef818ad82923ffcadbef
sathiya <sathiya@herbet.agency>
2024-08-09 11:21:51 +0530

    pff additional changes

--- components/PFF/Component/SlideImage.jsx
+++ components/PFF/Component/SlideImage.jsx
@@ -1,15 +1,24 @@
 /* eslint-disable */
 import React from 'react'
 
-const SlideImage = ({url}) => {
+const SlideImage = ({url,urlMob}) => {
   return (
     <div className="container flex flex-col  ">
     <div className="self-center ">
+      <div className="hidden md:block w-[100%]">
+        <img
+          src={url}
+          className="min-h-[100vh] min-w-[100vw] max-w-[100vw] "
+          alt="slider"
+        />
+      </div>
+      <div className="md:hidden">
       <img
-        src={url}
-        className="min-h-[100vh] min-w-[100vw]"
+        src={urlMob}
+        className=" object-contain"
         alt="slider"
       />
+    </div>
     </div>
   </div>
   )
