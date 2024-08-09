fc0a9807eb119451aaad169e6c0436ac35c4c08a
sathiya <sathiya@herbet.agency>
2024-08-09 11:27:55 +0530

    pff additional changes 1

--- components/PFF/Component/SlideImage.jsx
+++ components/PFF/Component/SlideImage.jsx
@@ -3,12 +3,12 @@ import React from 'react'
 
 const SlideImage = ({url,urlMob}) => {
   return (
-    <div className="container flex flex-col  ">
+    <div className=" flex flex-col min-w-[100vw] ">
     <div className="self-center ">
       <div className="hidden md:block w-[100%]">
         <img
           src={url}
-          className="min-h-[100vh] min-w-[100vw] max-w-[100vw] "
+          className="min-h-[100vh] min-w-[100vw] object-cover"
           alt="slider"
         />
       </div>
