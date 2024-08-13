83c59d88a1cd5f374a0ec2e7d2808245a3564d14
sathiya <sathiya@herbet.agency>
2024-08-13 21:06:18 +0530

    added the changes3

--- components/PFA/EventLayout.jsx
+++ components/PFA/EventLayout.jsx
@@ -3,11 +3,11 @@ import React from 'react'
 
 const EventLayout = () => {
   return (
-    <div className="flex flex-col">
+    <div className="flex flex-col gap-4 md:gap-0">
         <h1 className="text-[24px] md:text-[32px]  tracking-[1px] font-[Integral] font-[700] text-center self-center text-[#fff]  uppercase">EVENT LAYOUT</h1>
       <div className="self-center">
         <img src="./PFA/Layout/Layout.png" className="min-w-[90vw] hidden md:block object-none max-h-[550px]" alt="layout" />
-        <img src="./PFA/Layout/Layout_mob.png" className=" block md:hidden self-center object-contain max-h-[195px] max-w-[387px] " alt="layout" />
+        <img src="./PFA/Layout/Layout_mob_1.png" className=" block md:hidden self-center object-contain max-h-[195px] max-w-[350px] " alt="layout" />
       </div>
     </div>
   )
