e8c7668a2e940f205c7c1a8fb4ed25a84c6eb398
sathiya <sathiya@herbet.agency>
2024-08-13 09:30:09 +0530

    new changes

--- components/PFA/TicketGa.jsx
+++ components/PFA/TicketGa.jsx
@@ -10,8 +10,8 @@ const TicketGa = () => {
         <TicketHeadingLabel text="GENERAL ADMISSION (GA)" />
        
         <div className="flex flex-wrap justify-center  gap-3  min-w-[90vw] md:min-w-[60vw]">
-        <TicketContainer payLater="buy now pay later" color="pink" title="GENERAL ADMISSION" type="Earlybird" price="RM 198" buttonText="Buy Now" link1="#https://www.ticket2u.com.my/event/37375" link2="https://www.ticket2u.com.my/event/37445" />
-        <TicketContainer payLater="buy now pay later"  color="pink" title="GENERAL ADMISSION" type="phase 1" price="RM 228" buttonText="Buy Now" link1="#https://www.ticket2u.com.my/event/37375" link2="https://www.ticket2u.com.my/event/37445" />
+        <TicketContainer payLater="buy now pay later" color="pink" title="GENERAL ADMISSION" type="Earlybird" price="RM 198" buttonText="Buy Now" link1="https://www.ticket2u.com.my/event/37375" link2="https://www.ticket2u.com.my/event/37445" />
+        <TicketContainer payLater="buy now pay later"  color="pink" title="GENERAL ADMISSION" type="phase 1" price="RM 228" buttonText="Buy Now" link1="https://www.ticket2u.com.my/event/37375" link2="https://www.ticket2u.com.my/event/37445" />
         <TicketContainer color="gray" title="GENERAL ADMISSION" type="phase 2" price="RM 258"  buttonText="coming soon" link1="#" />
         
         
