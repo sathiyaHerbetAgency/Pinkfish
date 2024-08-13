95e94a4a5d94ec3de9dc81e769c1abde9f3ad9b2
sathiya <sathiya@herbet.agency>
2024-08-13 08:57:54 +0530

    button link updated 1

--- components/PFA/TicketVip.jsx
+++ components/PFA/TicketVip.jsx
@@ -9,7 +9,7 @@ const TicketVip = () => {
      
     <TicketHeadingLabel text="premium ADMISSION (vip)" />
     <div className="flex flex-wrap justify-center  gap-3  min-w-[90vw] md:min-w-[60vw]">
-    <TicketContainer payLater="buy now pay later" color="pink" title="vip" type="Earlybird" price="RM 328" buttonText="Buy tickets 13 aug, 12pm" link1="#https://www.ticket2u.com.my/event/37375" link2="https://www.ticket2u.com.my/event/37445" />
+    <TicketContainer payLater="buy now pay later" color="pink" title="vip" type="Earlybird" price="RM 328" buttonText="Buy Now" link1="#https://www.ticket2u.com.my/event/37375" link2="https://www.ticket2u.com.my/event/37445" />
     <TicketContainer color="gray" title="vip" type="phase 1" price="RM 358" buttonText="coming soon" link1="" />
     <TicketContainer color="gray" title="vip" type="phase 2" price="RM 388" buttonText="coming soon" link1="" />
 
