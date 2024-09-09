53123a28b00b4d074e7a5763e2b45cfb0bf001cc
sathiya <sathiya@herbet.agency>
2024-09-09 13:21:26 +0530

    home page optimization test 2

--- components/EventContainer.jsx
+++ components/EventContainer.jsx
@@ -16,23 +16,20 @@ const EventContainer = ({link,title,description}) => {
     }
 
   return (
-    <a href={link} className={` md:min-w-[300px] h-[80px] md:h-[120px] ${checkContents()} rounded-2xl flex flex-col  justify-center border md:border-none text-white`}>
+    <a href={link} arial-label={link} className={` md:min-w-[300px] h-[80px] md:h-[120px] ${checkContents()} rounded-2xl flex flex-col  justify-center border md:border-none text-white`}>
         <div className="flex flex-col justify-center ">
             <div className="self-center w-[90%] flex justify-between">
                 <div className="flex flex-col ">
                     <div className="flex items-center gap-2">
                         <h2 className="text-[24px] md:text-[40px] font-[Integral] font-[700]  text-left  text-[#F20D86]">{title}</h2>
-                        {/* {title==="#PFA"&&  
-                            <button type='button' className='uppercase text-[8px] md:text-[10px] font-[Sofia] font-[100]  h-fit text-center w-fit px-1 py-1 rounded-2xl  text-[#fff]  bg-[#FF0086]'> Coming Soon</button>
-                        } */}
                     </div>
                     <h2 className="text-[10px] md:text-[14px] font-[Integral] font-[300]  text-left  text-[#fff]">{description}</h2>
                 </div>
-                <a className="cursor-pointer">
+                <button className="cursor-pointer">
                     <svg width="17" height="17" viewBox="0 0 17 17" fill="none" xmlns="http://www.w3.org/2000/svg">
                         <path d="M12.9328 7.40717L7.2335 1.54526L8.73588 0L17 8.5L8.73588 17L7.2335 15.4547L12.9328 9.59283H0V7.40717H12.9328Z" fill="white"/>
                     </svg>
-                </a>
+                </button>
             </div>
         </div>   
     </a>
