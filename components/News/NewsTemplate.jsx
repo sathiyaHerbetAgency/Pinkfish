081b55273d19e8987c4ababaf94440f0e0ba9269
sathiya <sathiya@herbet.agency>
2024-09-09 15:18:35 +0530

    new issue fixed

--- components/News/NewsTemplate.jsx
+++ components/News/NewsTemplate.jsx
@@ -7,11 +7,11 @@ const NewsTemplate = ({header,paragraph,link,img}) => {
    <>
     {NewsData.map((each,index)=>
         <div key={index+1} className='basis-1/3 border rounded border-white flex flex-col gap-4'>
-        <img src={each.img} alt='news image' />
-        <div className='flex flex-col gap-4 self-center pb-4 w-[90%]'>
+        <img src={each.img} className="" alt='news image' />
+        <div className='flex flex-col gap-4 self-center pb-4 max-w-[90%]'>
         <h1 className='news_card_header '>{each.heading}</h1>
-        <p className='news_card_text'>{each.description}</p>
-        {each.description2&& <p className='news_card_text'>{each.description2}</p>}
+        <p className='news_card_text '>{each.description}</p>
+        {each.description2&& <p className='news_card_text '>{each.description2}</p>}
         <a href={each.link} target='_blank'>
         <button type='button' className='news_read_btn'>Read More</button>
         </a>
