66b9912f6ab33db88b7a91829194d138542bf764
sathiya <sathiya@herbet.agency>
2024-10-04 09:36:15 +0530

    added the new slide top home4

--- components/Home/Componets/SlideImage.jsx
+++ components/Home/Componets/SlideImage.jsx
@@ -11,7 +11,7 @@ const SlideImage = ({url,urlMob,year,link,slide}) => {
     {
       dateText:'31 December 2024 • Surf Beach Sunway Lagoon',
       heading:'pinkfish countdown 2024',
-      text:'Get ready for more sick beats and a dope night at our Pinkfish Countdown 2024 this December 31st.',
+      text:'Get ready to blast into 2025 at Pinkfish Countdown, where the beats drop as the clock strikes twelve!',
       link:'/countdown2024',
       ticketText:'Buy Tickets'
     },
