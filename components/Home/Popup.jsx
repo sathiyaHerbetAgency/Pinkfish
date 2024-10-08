72af1f59061dbf7c0b915264e130afd628aabf23
sathiya <sathiya@herbet.agency>
2024-10-08 12:08:25 +0530

    added the popup padding-changed3

--- components/Home/Popup.jsx
+++ components/Home/Popup.jsx
@@ -10,6 +10,8 @@ const Popup = () => {
 
     const imgRef = useRef(null); // Reference for the image
     const [containerHeight, setContainerHeight] = useState(0); // State for container height
+    const [containerWidth, setContainerWidth] = useState(0); // State for container height
+
   
     useEffect(() => {
       openPopup()
@@ -19,6 +21,8 @@ const Popup = () => {
         if (imgRef.current) {
           // Set the container height based on the image's height
           setContainerHeight(imgRef.current.clientHeight);
+          setContainerWidth(imgRef.current.clientWidth);
+
         }
       };
   
@@ -117,8 +121,8 @@ const Popup = () => {
         />
       </div>
       <div className="popup-card_main  ">
-        <div style={{ height: containerHeight }} className="flex flex-col h-[380px]  pb-8 justify-end absolute">
-          <div className="flex self-center w-[300px] gap-3 justify-center">
+        <div style={{ height: containerHeight, width:containerWidth }} className="flex flex-col h-[380px]  pb-4 justify-end absolute">
+          <div style={{ width:containerWidth }} className="flex self-center w-[300px] gap-3 justify-center">
           <a href="#" className="w-fit self-center flex justify-start mt-3"> 
                 <button className={button} alt="button">Buy Now</button>
               </a>
