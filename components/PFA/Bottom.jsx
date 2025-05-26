0215dac6360824c5cf8672284dff733e8920c76d
sathiya <sathiya@herbet.agency>
2025-05-26 18:29:30 +0530

    fixed

--- components/PFA/Bottom.jsx
+++ components/PFA/Bottom.jsx
@@ -12,11 +12,11 @@ const Bottom = () => {
     <div className="bg-bottom-pfa h-[60vh] md:h-[90vh]  py-12   flex flex-col justify-center gap-6">
         <div className="flex flex-col gap-6 w-[70%] self-center">
         <img src="./PFA/pf_arena_logo.png" alt="logo" className="self-center w-[150px] md:w-[250px]"/>
-          <div className="flex flex-col self-center md:w-[70%] items-center gap-6">
+          <div className="flex flex-col self-center w-full md:w-[70%] items-center gap-6 ">
             {/* <h3 className="text-[24px] md:text-[34px]  font-[Integral] text-center  text-[#FF0086]">{heading}</h3> */}
             <p className="text-[18px] md:text-[24px]  font-[manrope] text-center  text-[#fff]"><span className="font-bold">{textBold}</span> {text}</p>
-            <img src="./PFA/Nifra/sponcer.png"  className="w-[300px] self-center" alt="" />
-            <button onClick={()=>{scrollTop()}} className='top_btn font-[400] flex border border-[#fff] p-3 px-6 rounded-lg  self-center mb-11 '>
+              <img src="./PFA/Nifra/sponcer.png" class="self-center h-16 md:h-none w-[240px] md:w-[200px]" alt="" />
+              <button onClick={()=>{scrollTop()}} className='top_btn font-[400] flex border border-[#fff] p-3 px-6 rounded-lg  self-center mb-11 '>
                 back to top 
               <img className='mt-0 md:mt-1 ml-2' src='/uparrow.png' alt="up-arrow icon" />
 
